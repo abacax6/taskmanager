71ed3aaaae36915fcd8f200863942b2ff917bb56
AbacaX <victorraze@protonmail.com>
2026-05-13 16:04:40 -0300

    fix: Correct HTTP code response to status validation

--- validators.py
+++ validators.py
@@ -2,7 +2,8 @@ VALID_STATUS = ["to do", "in progress", "completed"]
 
 def validate_status(status: str):
     if status not in VALID_STATUS:
-        raise ValueError("Invalid status")    
+        #raise ValueError("Invalid status")  
+        raise HTTPException(status_code=400, detail="Invalid status")  
 
 def validate_title(title: str):
     if not title.strip(): #Verificação para título vazio.
