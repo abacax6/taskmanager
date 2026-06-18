2c0bd2738b09ac497cb6e964298a713720a14abe
AbacaX <victorraze@protonmail.com>
2026-06-17 21:50:50 -0300

    refactor: update get_current_user and create_access_token

--- auth.py
+++ auth.py
@@ -1,3 +1,6 @@
+from fastapi.security import (
+    HTTPBearer
+)
 from passlib.context import CryptContext
 from database import users_collection
 from jose import jwt
@@ -6,6 +9,11 @@ from datetime import (
     timedelta
 )
 import os
+from fastapi import (
+    Depends,
+    HTTPException
+) 
+
 
 SECRET_KEY = os.getenv(
     "SECRET_KEY"
@@ -17,6 +25,8 @@ pwd_context = CryptContext(
     schemes=["bcrypt"],
     deprecated="auto"
 )
+
+oauth2_scheme = HTTPBearer()
 
 def hash_password(password):
     return pwd_context.hash(password)
@@ -61,7 +71,7 @@ def create_access_token(
     )
 
     payload = {
-        "sub": user["email"],
+        "sub": str(user["id"]),
         "exp": expire
     }
 
@@ -70,3 +80,45 @@ def create_access_token(
         SECRET_KEY,
         algorithm=ALGORITHM
     )
+
+def get_current_user(
+    credentials=Depends(oauth2_scheme)
+):
+
+    token = credentials.credentials
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email = payload.get("sub")
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        user = users_collection.find_one(
+            {"id": int(user_id)}
+        )
+
+        if user is None:
+            raise HTTPException(
+                status_code=401,
+                detail="User not found"
+            )
+
+        return user
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=401,
+            detail=str(e)
+        )
